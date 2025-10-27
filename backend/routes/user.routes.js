1282f60d0c169789d99cf154d3f318e29f09a886
AnkitShaw-100 <ankitshaw6933@gmail.com>
2025-10-27 18:26:41 +0530

    login route

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -17,4 +17,11 @@ router.post(
     userController.registerUser
 );
 
+router.post("/login",
+    [
+        body("email").isEmail().withMessage("Invalid Email"),
+        body('password').isLength({ min: 6 }).withMessage("Password is not correct!")
+    ],
+    userController.loginUser
+)
 module.exports = router;
